b46be9cac4aeffa66b8b31b51fbc8f21fd5bbe3d
TyagiSumit <sumittyagi1998@gmail.com>
2019-08-31 13:20:58 +0530

    query segment tree

--- segment_tree/segment_tree.py
+++ segment_tree/segment_tree.py
@@ -29,9 +29,26 @@ def update_tree(arr, tree, start, end, tree_node, idx, val):
     
     tree[tree_node] = tree[2*tree_node] + tree[2*tree_node +1]
 
+def query(tree, start, end, tree_node, left, right):
+
+    """ completely outside given range """
+    if start > right or end < left:
+        return 0
+
+    """ Completely inside the given range """
+    if start >= left and end <= right:
+        return tree[tree_node]
+
+    """ Partially inside and partially outside """
+    mid = (start + end)//2
+    ans1 = query(tree, start, mid, 2*tree_node, left, right)
+    ans2 = query(tree, mid+1, end, 2*tree_node, left, right)
+    return ans1 + ans2
 
 
 buildTree(arr, tree, 0, len(arr)-1, 1)
 update_tree(arr, tree, 0, len(arr)-1, 1, 2, 10)
 
-print(*tree[1:]) 
+ans= query(tree,0,4,1,2,4)
+print(ans)
+print(*tree[1:])
