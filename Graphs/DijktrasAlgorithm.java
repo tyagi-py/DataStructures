690413bf472e868861725ac9f8175f4009af7cd1
TyagiSumit <sumittyagi1998@gmail.com>
2020-03-27 12:03:38 +0530

    Added Dijkstra and Prim's

--- Graphs/DijktrasAlgorithm.java
+++
@@ -1,9 +0,0 @@
-package Graphs;
-
-public class DijktrasAlgorithm {
-    public static void main(String args[]){
-        WeightedGraph graph = new WeightedGraph();
-        graph.getGraph();
-
-    }
-}
