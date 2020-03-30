7194ea48612df7483890277ffc29b87254a00f4a
TyagiSumit <sumittyagi1998@gmail.com>
2020-03-30 12:14:01 +0530

    Update Dijkstra and Prims

--- Graphs/Dijkstra.java
+++ Graphs/Dijkstra.java
@@ -1,96 +1,64 @@
-// A Java program for Dijkstra's single source shortest path algorithm.
-// The program is for adjacency matrix representation of the graph
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.util.Scanner;
 
-class Dijkstra {
-    // A utility function to find the vertex with minimum distance value,
-    // from the set of vertices not yet included in shortest path tree
-    static final int V = 9;
-    int minDistance(int dist[], Boolean sptSet[])
-    {
-        // Initialize min value
-        int min = Integer.MAX_VALUE, min_index = -1;
+public class Dijkstra {
 
-        for (int v = 0; v < V; v++)
-            if (sptSet[v] == false && dist[v] <= min) {
-                min = dist[v];
-                min_index = v;
+    private static void dijkstra(int[][] adjacencyMatrix) {
+        int v = adjacencyMatrix.length;
+        boolean visited[] = new boolean[v];
+        int distance[] = new int[v];
+        distance[0] = 0;
+        for(int i = 1; i < v; i++){
+            distance[i] = Integer.MAX_VALUE;
+        }
+
+        for(int i = 0; i < v - 1; i++){
+            // Find Vertex with Min distance
+            int minVertex = findMinVertex(distance, visited);
+            visited[minVertex] = true;
+            // Explore neighbors
+            for(int j = 0; j < v; j++){
+                if(adjacencyMatrix[minVertex][j] != 0 && !visited[j] && distance[minVertex] != Integer.MAX_VALUE){
+                    int newDist = distance[minVertex] + adjacencyMatrix[minVertex][j];
+                    if(newDist < distance[j]){
+                        distance[j] = newDist;
+                    }
+                }
             }
-
-        return min_index;
-    }
-
-    // A utility function to print the constructed distance array
-    void printSolution(int dist[])
-    {
-        System.out.println("Vertex \t\t Distance from Source");
-        for (int i = 0; i < V; i++)
-            System.out.println(i + " \t\t " + dist[i]);
-    }
-
-    // Function that implements Dijkstra's single source shortest path
-    // algorithm for a graph represented using adjacency matrix
-    // representation
-    void dijkstra(int graph[][], int src)
-    {
-        int dist[] = new int[V]; // The output array. dist[i] will hold
-        // the shortest distance from src to i
-
-        // sptSet[i] will true if vertex i is included in shortest
-        // path tree or shortest distance from src to i is finalized
-        Boolean sptSet[] = new Boolean[V];
-
-        // Initialize all distances as INFINITE and stpSet[] as false
-        for (int i = 0; i < V; i++) {
-            dist[i] = Integer.MAX_VALUE;
-            sptSet[i] = false;
         }
 
-        // Distance of source vertex from itself is always 0
-        dist[src] = 0;
-
-        // Find shortest path for all vertices
-        for (int count = 0; count < V - 1; count++) {
-            // Pick the minimum distance vertex from the set of vertices
-            // not yet processed. u is always equal to src in first
-            // iteration.
-            int u = minDistance(dist, sptSet);
-
-            // Mark the picked vertex as processed
-            sptSet[u] = true;
-
-            // Update dist value of the adjacent vertices of the
-            // picked vertex.
-            for (int v = 0; v < V; v++)
-
-                // Update dist[v] only if is not in sptSet, there is an
-                // edge from u to v, and total weight of path from src to
-                // v through u is smaller than current value of dist[v]
-                if (!sptSet[v] && graph[u][v] != 0 && dist[u] != Integer.MAX_VALUE && dist[u] + graph[u][v] < dist[v])
-                    dist[v] = dist[u] + graph[u][v];
+        // Print
+        for(int i = 0; i < v; i++){
+            System.out.println(i + " "+ distance[i]);
         }
 
-        // print the constructed distance array
-        printSolution(dist);
     }
 
-    // Driver method
-    public static void main(String[] args)
-    {
-        /* Let us create the example graph discussed above */
-        int graph[][] = new int[][] { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-                { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-                { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-                { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-                { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-                { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-                { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-                { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-                { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
-        Dijkstra t = new Dijkstra();
-        t.dijkstra(graph, 0);
+    private static int findMinVertex(int[] distance, boolean visited[]) {
+
+        int minVertex = -1;
+        for(int i = 0; i < distance.length; i++){
+            if(!visited[i]  && (minVertex == -1 || distance[i] < distance[minVertex])){
+                minVertex = i;
+            }
+        }
+        return minVertex;
     }
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int v = s.nextInt();
+        int e = s.nextInt();
+        int adjacencyMatrix[][] = new int[v][v];
+        for(int i = 0; i < e; i++){
+            int v1 = s.nextInt();
+            int v2 = s.nextInt();
+            int weight = s.nextInt();
+            adjacencyMatrix[v1][v2] = weight;
+            adjacencyMatrix[v2][v1] = weight;
+        }
+        dijkstra(adjacencyMatrix);
+    }
+
+
 }
-// This code is contributed by Aakash Hasija
